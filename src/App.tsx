8d6ffbe57e66846cec31f4080b5dade357c9fab2
Halyna Kravchenko <108605949+minimal005@users.noreply.github.com>
2025-02-26 16:22:06 +0200

    fix bg color

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { SearchForm } from "./components/SearchForm";
 import { Container } from "./components/Container";
 import { TodoList } from "./components/TodoList";
 import { useAppSelector } from "./app/hooks";
-import "./App.css";
-// import { ColorModeButton } from "./components/ui/color-mode";
 import { Flex } from "@chakra-ui/react";
 import ColorModeButton from "./components/styleButton";
+import "./App.css";
 
 export const App = () => {
   const { open, done, inProgress } = useAppSelector((state) => state.issues);
